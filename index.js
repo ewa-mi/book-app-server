145d4c6783ac883ab1e3cdae9a6ea7dbe497af7e
ewami <efili1989@gmail.com>
2020-06-18 12:03:30 +0200

    set up auth - jwt, middleware, create login endpoint

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
+const authRouter = require("./routers/auth");
 const { PORT } = require("./config/constants");
 const cors = require("cors");
+const bodyParserMiddleWare = express.json();
 const bookscollectionRouter = require("./routers/bookscollection");
 const userRouter = require("./routers/user");
 
+app.use(bodyParserMiddleWare);
 app.use(cors());
 
-// app.use(express.json());
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
 
 app.use("/bookscollection", bookscollectionRouter);
 app.use("/user", userRouter);
+app.use("/", authRouter);
 
 app.listen(PORT, console.log(`server running on ${PORT}`));
---
+++ routers/auth.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt");
+const { Router } = require("express");
+const { toJWT } = require("../auth/jwt");
+const User = require("../models/").user;
+
+const router = new Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide email and password" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
+      });
+    }
+
+    delete user.dataValues["password"];
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong" });
+  }
+});
+
+module.exports = router;
